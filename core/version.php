f1ef1bd9420e0d23cc72d05fffd3e8007d314e9a
tytanik2011 <tytanik2011@gmail.com>
2017-03-13 22:44:41 +0100

    0.0.3
    Repeaired old version checker

--- core/version.php
+++ core/version.php
@@ -3,9 +3,9 @@
     $serverVersionPrepare = json_decode(file_get_contents("https://raw.githubusercontent.com/tytanik2011/McPage/0.1/core/version.json"), true);
     $serverVersion = $serverVersionPrepare['version'];
     if($clientVersion != $serverVersion){
-        echo "<script>alert('Wersja strony jest nieaktualna!')";
+        echo '<script>alert("Wersja strony jest nieaktualna!")</script>';
         $returnedVersion = $clientVersion . " NIEAKTUALNA!";
     }else{
-         $returnedVersion = $serverVersion;   
+         $returnedVersion = $serverVersion;
     }
 ?>
