f53722996fc12cea40f40a527f853a87747e5d38
tytanik2011 <tytanik2011@gmail.com>
2017-03-13 22:36:53 +0100

    0.0.2
    Added version checker

--- core/version.php
+++ core/version.php
@@ -1,5 +1,11 @@
 <?php
     $clientVersion = "0.1";
-    $serverVersion = json_decode(file_get_contents("https://mcapi.ca/query/szesciennyglob.pl:25565/info"), true);
-    $returnedVersion = "0.1";
+    $serverVersionPrepare = json_decode(file_get_contents("https://raw.githubusercontent.com/tytanik2011/McPage/0.1/core/version.json"), true);
+    $serverVersion = $serverVersionPrepare['version'];
+    if($clientVersion != $serverVersion){
+        echo "<script>alert('Wersja strony jest nieaktualna!')";
+        $returnedVersion = $clientVersion . " NIEAKTUALNA!";
+    }else{
+         $returnedVersion = $serverVersion;   
+    }
 ?>
